459c19618976e25aa9422ccf573fbf218cbb1a85
technexx <48839830+technexx@users.noreply.github.com>
2020-06-29 20:36:56 -0700

    filling it out

--- app/src/main/java/technexx/android/realcost/MainActivity.java
+++ app/src/main/java/technexx/android/realcost/MainActivity.java
@@ -11,10 +11,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText income_edit;
     private EditText rent_edit;
     private EditText bills_edit;
     private EditText misc_edit;
     private TextView total_edit;
+    private String income;
     private String rent;
     private String bills;
     private String misc;
@@ -24,10 +26,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        income_edit = findViewById(R.id.income_edit);
         rent_edit = findViewById(R.id.rent_edit);
         bills_edit = findViewById(R.id.bills_edit);
         misc_edit = findViewById(R.id.misc_edit);
         total_edit = findViewById(R.id.total_edit);
+
+        income_edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                convert();
+            }
+        });
 
         rent_edit.addTextChangedListener(new TextWatcher() {
             @Override
@@ -38,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public void afterTextChanged(Editable s) {
-                total_edit.setText(convert());
+                convert();
             }
         });
 
@@ -53,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                total_edit.setText(convert());
+                convert();
             }
         });
 
@@ -68,20 +84,27 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                total_edit.setText(convert());
+                convert();
             }
         });
 
     }
 
-    private String convert() {
+    private void convert() {
+        income = income_edit.getText().toString();
         rent = rent_edit.getText().toString();
         bills = bills_edit.getText().toString();
         misc = misc_edit.getText().toString();
 
+        int incomeVal = 0;
         int rentVal = 0;
         int billsVal = 0;
         int miscVal = 0;
+        int postExpenses = 0;
+
+        if(!income.equals("")) {
+            incomeVal = Integer.parseInt(income);
+        }
         if (!rent.equals("")) {
             rentVal = Integer.parseInt(rent);
         }
@@ -92,6 +115,13 @@ public class MainActivity extends AppCompatActivity {
             miscVal = Integer.parseInt(misc);
         }
 
-        return String.valueOf(rentVal + billsVal + miscVal);
+        if (!income.equals("") && !rent.equals("") && !bills.equals("") && !misc.equals("")) {
+            postExpenses = (incomeVal - (rentVal + billsVal + miscVal));
+            if (postExpenses <0) {
+                postExpenses = 0;
+            }
+            total_edit.setText(String.valueOf(postExpenses));
+        }
+
     }
 }
