043c293e05d2a1bf77356d0dd726a71f0e98f893
technexx <48839830+technexx@users.noreply.github.com>
2020-06-30 19:50:20 -0700

    added savedpref

--- app/src/main/java/technexx/android/realcost/MainActivity.java
+++ app/src/main/java/technexx/android/realcost/MainActivity.java
@@ -2,6 +2,7 @@ package technexx.android.realcost;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -28,12 +29,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences pref = getSharedPreferences("Pref", 0);
+        SharedPreferences.Editor editor = pref.edit();
+
+        int savedIncome = pref.getInt("income", 0);
+        int savedExpenses = pref.getInt("expenses", 0);
+        int postExpenses = pref.getInt("postExpenses", 0);
+
         income_edit = findViewById(R.id.income_edit);
         expenses_edit = findViewById(R.id.expenses_edit);
         net_income = findViewById(R.id.net_income);
 
         purchase_cost = findViewById(R.id.purchase_cost);
         actual_cost = findViewById(R.id.actual_cost);
+
+        income_edit.setText(String.valueOf(savedIncome));
+        expenses_edit.setText(String.valueOf(savedExpenses));
+        net_income.setText(String.valueOf(postExpenses));
+
+        income_edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                convert();
+            }
+        });
 
         expenses_edit.addTextChangedListener(new TextWatcher() {
             @Override
@@ -63,10 +88,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void convert() {
+        SharedPreferences pref = getSharedPreferences("Pref", 0);
+        SharedPreferences.Editor editor = pref.edit();
+
         String income = income_edit.getText().toString();
         String expense = expenses_edit.getText().toString();
 
-        if(!income.equals("")) {
+        if(!income.equals("") && !expense.equals("")) {
             incomeVal = Integer.parseInt(income);
             expenseVal = Integer.parseInt(expense);
             postExpenses = (incomeVal - expenseVal);
@@ -74,6 +102,10 @@ public class MainActivity extends AppCompatActivity {
                 postExpenses = 0;
             }
             net_income.setText(String.valueOf(postExpenses));
+            editor.putInt("income", incomeVal);
+            editor.putInt("expenses", expenseVal);
+            editor.putInt("postExpenses", postExpenses);
+            editor.apply();
         }
     }
 
